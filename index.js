5f4c7e5cd09d707e8e75a4ddf9077bd72e11dfd4
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-02 12:03:27 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = []; 
+  
+    for (let i = 0; i < names.length; i++) {  
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; 
+      messages.push(message);  
+    }
+  
+    return messages; 
+  }
+  
+  module.exports = writeCards;
+
+  function countDown(number) {
+    while (number >= 0) {  // Loop from number down to 0
+      console.log(number);
+      number--;  // Decrease the number by 1 each time
+    }
+  }
+  
+  // Export the function for testing
+  module.exports = countDown;
